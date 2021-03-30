fdba668f62423cab867ad67ff95c3366791ff668
Tom Fletcher <tom@tomfletcher.tech>
2021-03-30 22:08:55 +0100

    Init

---
+++ Client/Helpers/StringUtilities.cs
@@ -0,0 +1,8 @@
+namespace BlazorMovieStore.Client.Helpers
+{
+    public class StringUtilities
+    {
+    
+        public static string CustomToUpper(string value) => value.ToUpper();
+    }
+}
---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorMovieStore.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
